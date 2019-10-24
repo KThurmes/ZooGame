3444b9b299c35c43a9eecd12402394c018b6339e
Kathleen Thurmes <thurmesk@oregonstate.edu>
2019-10-24 16:05:17 -0700

    just trying to access my files.

--- NewCritter.hpp
+++ NewCritter.hpp
@@ -1,10 +1,13 @@
 #ifndef NEWCRITTER_HPP
 #define NEWCRITTER_HPP 
 
+#include "Animal.hpp"
+
 class NewCritter: public Animal{
     private:
-    
+        double foodCost;
     public:
+        NewCritter();
 };
 
 #endif
